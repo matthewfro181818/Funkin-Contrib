b1453f15c22b34c16a71735b51daaedc945dc5cc
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-09-09 18:19:56 -0400

    Add files via upload

--- source/funkin/ui/debug/char/components/dialogs/DefaultPageDialog.hx
+++ source/funkin/ui/debug/char/components/dialogs/DefaultPageDialog.hx
@@ -1,27 +1,22 @@
 package funkin.ui.debug.char.components.dialogs;
 
-import haxe.ui.containers.dialogs.CollapsibleDialog;
+import flixel.group.FlxSpriteGroup;
 
-class DefaultPageDialog extends CollapsibleDialog
-{
-  public var page:CharCreatorDefaultPage = null;
-
-  override public function new(page:CharCreatorDefaultPage)
-  {
-    super();
-
-    modal = false;
-    destroyOnClose = false;
-    this.page = page;
-  }
-
-  override public function set_hidden(value:Bool)
-  {
-    var playSound = (hidden != value);
-
-    if (playSound && !value) funkin.audio.FunkinSound.playOnce(Paths.sound('chartingSounds/openWindow'));
-    else if (playSound && value) funkin.audio.FunkinSound.playOnce(Paths.sound('chartingSounds/exitWindow'));
-
-    return super.set_hidden(value);
-  }
+class DefaultPageDialog extends FlxSpriteGroup {
+    public var hidden:Bool = true;
+    public function new() {
+        super();
+        visible = !hidden;
+    }
+    public function showDialog():Void {
+        hidden = false;
+        visible = true;
+    }
+    public function hideDialog():Void {
+        hidden = true;
+        visible = false;
+    }
+    public function destroy():Void {
+        super.destroy();
+    }
 }
---
+++ source/funkin/ui/debug/char/handlers/import.hx
@@ -0,0 +1,4 @@
+package funkin.ui.debug.char.handlers;
+
+// placeholder import aggregator for 0.7.4 backport
+class import {}
