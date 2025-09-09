4d1975b6f0b66f4aa378fb778f6eec629671b9e9
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-09-09 18:28:48 -0400

    Add files via upload

--- source/funkin/ui/debug/char/components/dialogs/freeplay/FreeplayDJSettingsDialog.hx
+++ source/funkin/ui/debug/char/components/dialogs/freeplay/FreeplayDJSettingsDialog.hx
@@ -1,29 +1,63 @@
 package funkin.ui.debug.char.components.dialogs.freeplay;
 
-import funkin.ui.debug.char.components.dialogs.DefaultPageDialog;
-import funkin.ui.debug.char.components.common.NumberField;
+import funkin.ui.debug.char.pages.CharCreatorFreeplayPage;
+import funkin.data.freeplay.player.PlayerRegistry;
 
-class FreeplayDJSettingsDialog extends DefaultPageDialog {
-    // Background scrolling texts
-    public var bgText1:String = "FUNKIN";
-    public var bgText2:String = "FREEPLAY";
-    public var bgText3:String = "DJ";
-    // Timings / frames (as NumberFields exposing .pos)
-    public var introStartFrame:NumberField = new NumberField(0);
-    public var introEndFrame:NumberField = new NumberField(0);
-    public var loopStartFrame:NumberField = new NumberField(0);
-    public var loopEndFrame:NumberField = new NumberField(0);
-    public var introBadStartFrame:NumberField = new NumberField(0);
-    public var introBadEndFrame:NumberField = new NumberField(0);
-    public var loopBadStartFrame:NumberField = new NumberField(0);
-    public var loopBadEndFrame:NumberField = new NumberField(0);
+@:build(haxe.ui.macros.ComponentMacros.build("assets/exclude/data/ui/char-creator/dialogs/freeplay/dj-settings-dialog.xml"))
+@:access(funkin.ui.debug.char.pages.CharCreatorFreeplayPage)
+class FreeplayDJSettingsDialog extends DefaultPageDialog
+{
+  public var bgText1(get, never):String;
 
-    public var charSelectTransitionDelay:NumberField = new NumberField(0);
-    public var soundClickFrame:NumberField = new NumberField(0);
-    public var soundCartoonFrame:NumberField = new NumberField(0);
-    public var loopBlinkFrame:NumberField = new NumberField(0);
-    public var loopFrame:NumberField = new NumberField(0);
-    public var channelChangeFrame:NumberField = new NumberField(0);
+  function get_bgText1():String
+  {
+    return bgTextField1.value ?? bgTextField1.placeholder;
+  }
 
-    public function new() { super(); }
+  public var bgText2(get, never):String;
+
+  function get_bgText2():String
+  {
+    return bgTextField2.value ?? bgTextField2.placeholder;
+  }
+
+  public var bgText3(get, never):String;
+
+  function get_bgText3():String
+  {
+    return bgTextField3.value ?? bgTextField3.placeholder;
+  }
+
+  override public function new(daPage:CharCreatorFreeplayPage)
+  {
+    super(daPage);
+
+    var data = daPage.data;
+
+    var currentChar = PlayerRegistry.instance.fetchEntry(data.importedPlayerData);
+    if (currentChar?.getFreeplayDJData() != null)
+    {
+      bgTextField1.value = currentChar.getFreeplayDJText(1);
+      bgTextField2.value = currentChar.getFreeplayDJText(2);
+      bgTextField3.value = currentChar.getFreeplayDJText(3);
+
+      introStartFrame.pos = currentChar.getFreeplayDJData().getFistPumpIntroStartFrame();
+      introEndFrame.pos = currentChar.getFreeplayDJData().getFistPumpIntroEndFrame();
+      loopStartFrame.pos = currentChar.getFreeplayDJData().getFistPumpLoopStartFrame();
+      loopEndFrame.pos = currentChar.getFreeplayDJData().getFistPumpLoopEndFrame();
+      introBadStartFrame.pos = currentChar.getFreeplayDJData().getFistPumpIntroBadStartFrame();
+      introBadEndFrame.pos = currentChar.getFreeplayDJData().getFistPumpIntroBadEndFrame();
+      loopBadStartFrame.pos = currentChar.getFreeplayDJData().getFistPumpLoopBadStartFrame();
+      loopBadEndFrame.pos = currentChar.getFreeplayDJData().getCharSelectTransitionDelay();
+
+      charSelectTransitionDelay.pos = currentChar.getFreeplayDJData().getCharSelectTransitionDelay();
+      soundClickFrame.pos = currentChar.getFreeplayDJData().getCartoonSoundClickFrame();
+      soundCartoonFrame.pos = currentChar.getFreeplayDJData().getCartoonSoundCartoonFrame();
+      loopBlinkFrame.pos = currentChar.getFreeplayDJData().getCartoonLoopBlinkFrame();
+      loopFrame.pos = currentChar.getFreeplayDJData().getCartoonLoopFrame();
+      channelChangeFrame.pos = currentChar.getFreeplayDJData().getCartoonChannelChangeFrame();
+    }
+
+    bgTextField1.onChange = bgTextField2.onChange = bgTextField3.onChange = _ -> daPage.updateScrollingTexts();
+  }
 }
--- source/funkin/ui/debug/char/components/wizard/RenderWizardDialog.hx
+++ source/funkin/ui/debug/char/components/wizard/RenderWizardDialog.hx
@@ -1,5 +1,32 @@
 package funkin.ui.debug.char.components.wizard;
 
-class RenderWizardDialog extends DefaultWizardDialog {
-    public function new() { super(); }
+import funkin.data.character.CharacterData.CharacterRenderType;
+
+@:build(haxe.ui.macros.ComponentMacros.build("assets/exclude/data/ui/char-creator/wizard/sprite-support.xml"))
+class RenderWizardDialog extends DefaultWizardDialog
+{
+  override public function new()
+  {
+    super(SELECT_CHAR_TYPE);
+
+    renderOptionSparrow.onChange = function(_) params.renderType = CharacterRenderType.Sparrow;
+    renderOptionPacker.onChange = function(_) params.renderType = CharacterRenderType.Packer;
+    renderOptionAtlas.onChange = function(_) params.renderType = CharacterRenderType.AnimateAtlas;
+    renderOptionMulti.onChange = function(_) params.renderType = CharacterRenderType.MultiSparrow;
+  }
+
+  override public function showDialog(modal:Bool = true)
+  {
+    super.showDialog(modal);
+    renderOptionSparrow.disabled = renderOptionPacker.disabled = renderOptionAtlas.disabled = renderOptionMulti.disabled = (!params.generateCharacter
+      || params.importedCharacter != null);
+
+    renderOptionSparrow.selected = params.renderType == CharacterRenderType.Sparrow;
+    renderOptionPacker.selected = params.renderType == CharacterRenderType.Packer;
+    renderOptionAtlas.selected = params.renderType == CharacterRenderType.AnimateAtlas;
+    renderOptionMulti.selected = params.renderType == CharacterRenderType.MultiSparrow;
+  }
+
+  override public function isNextStepAvailable()
+    return true;
 }
