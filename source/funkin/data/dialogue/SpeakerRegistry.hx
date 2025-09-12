a270f823af0834c6604cc70a57cab3a132a7a3ad
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-09-12 08:16:19 -0400

    Update SpeakerRegistry.hx

--- source/funkin/data/dialogue/SpeakerRegistry.hx
+++ source/funkin/data/dialogue/SpeakerRegistry.hx
@@ -1,53 +1,26 @@
 package funkin.data.dialogue;
 
-import funkin.data.BaseRegistry.IRegistryEntry;
+import funkin.play.cutscene.dialogue.Speaker;
+import funkin.play.cutscene.dialogue.ScriptedSpeaker;
+import funkin.util.tools.ISingleton;
+import funkin.data.DefaultRegistryImpl;
 
-/**
- * The data structure for a dialogue speaker.
- * Stored in JSON and loaded by the SpeakerRegistry.
- */
-typedef SpeakerData =
+@:nullSafety
+class SpeakerRegistry extends BaseRegistry<Speaker, SpeakerData> implements ISingleton implements DefaultRegistryImpl
 {
   /**
-   * The display name of the speaker.
+   * The current version string for the speaker data format.
+   * Handle breaking changes by incrementing this value
+   * and adding migration to the `migrateSpeakerData()` function.
    */
-  var name:String;
+  public static final SPEAKER_DATA_VERSION:thx.semver.Version = "1.0.0";
 
-  /**
-   * The portrait or avatar asset ID for the speaker.
-   */
-  var portrait:String;
+  public static final SPEAKER_DATA_VERSION_RULE:thx.semver.VersionRule = "1.0.x";
 
-  /**
-   * An optional voice or sound cue to use when the speaker is talking.
-   */
-  @:optional var voice:String;
-
-  /**
-   * Additional metadata for modders.
-   */
-  @:optional var metadata:Dynamic;
-}
-
-/**
- * A single entry in the SpeakerRegistry.
- */
-@:build(funkin.util.macro.RegistryMacro.buildEntry())
-class DialogueSpeaker implements IRegistryEntry<SpeakerData>
-{
-  public function new(id:String)
+  public function new()
   {
-    this.id = id;
-    this._data = _fetchData(id);
-  }
-
-  public final id:String;
-  public final _data:Null<SpeakerData>;
-
-  public var data(get, never):SpeakerData;
-  inline function get_data():SpeakerData
-  {
-    if (_data == null) throw 'DialogueSpeaker $id has no data!';
-    return _data;
+    super('SPEAKER', 'dialogue/speakers', SPEAKER_DATA_VERSION_RULE);
   }
 }
+
+typedef SpeakerEntryParams = {}
