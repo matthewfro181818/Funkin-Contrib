FunkinCrew/Funkin

chore: Add null safety to `PlayState` and `LoadingState`

source/funkin/ui/transition/LoadingState.hx
@@ -11,6 +11,7 @@ import funkin.graphics.shaders.ScreenWipeShader;
 import funkin.play.PlayState;
 import funkin.play.PlayStatePlaylist;
 import funkin.play.song.Song.SongDifficulty;
+import funkin.play.stage.Stage;
 import haxe.io.Path;
 import lime.app.Future;
 import lime.app.Promise;
@@ -20,6 +21,7 @@ import lime.utils.Assets as LimeAssets;
 import openfl.filters.ShaderFilter;
 import openfl.utils.Assets as OpenFLAssets;
 
+@:nullSafety
 class LoadingState extends MusicBeatSubState
 {
   inline static var MIN_TIME = 1.0;
@@ -29,18 +31,21 @@ class LoadingState extends MusicBeatSubState
   var target:NextState;
   var playParams:Null<PlayStateParams>;
   var stopMusic:Bool = false;
-  var callbacks:MultiCallback;
+  var callbacks:Null<MultiCallback>;
   var danceLeft:Bool = false;
 
   var loadBar:FlxSprite;
   var funkay:FlxSprite;
 
-  function new(target:NextState, stopMusic:Bool, playParams:Null<PlayStateParams> = null)
+  function new(target:NextState, stopMusic:Bool, ?playParams:Null<PlayStateParams>)
   {
     super();
     this.target = target;
     this.playParams = playParams;
     this.stopMusic = stopMusic;
+
+    this.loadBar = new FunkinSprite(0, FlxG.height - 20).makeSolidColor(0, 10, 0xFFff16d2);
+    this.funkay = FunkinSprite.create('funkay');
   }
 
   override function create():Void
@@ -48,14 +53,12 @@ class LoadingState extends MusicBeatSubState
     var bg:FunkinSprite = new FunkinSprite().makeSolidColor(FlxG.width, FlxG.height, 0xFFcaff4d);
     add(bg);
 
-    funkay = FunkinSprite.create('funkay');
     funkay.setGraphicSize(0, FlxG.height);
     funkay.updateHitbox();
     add(funkay);
     funkay.scrollFactor.set();
     funkay.screenCenter();
 
-    loadBar = new FunkinSprite(0, FlxG.height - 20).makeSolidColor(0, 10, 0xFFff16d2);
     add(loadBar);
 
     initSongsManifest().onComplete(function(lib) {
@@ -65,15 +68,16 @@ class LoadingState extends MusicBeatSubState
       if (playParams != null)
       {
         // Load and cache the song's charts.
-        if (playParams.targetSong != null)
-        {
-          playParams.targetSong.cacheCharts(true);
-        }
+        playParams.targetSong.cacheCharts(true);
 
         // Preload the song for the play state.
         var difficulty:String = playParams.targetDifficulty ?? Constants.DEFAULT_DIFFICULTY;
         var variation:String = playParams.targetVariation ?? Constants.DEFAULT_VARIATION;
-        var targetChart:SongDifficulty = playParams.targetSong?.getDifficulty(difficulty, variation);
+        var targetChart:Null<SongDifficulty> = playParams.targetSong.getDifficulty(difficulty, variation);
+        if (targetChart == null)
+        {
+          throw 'Couldn\'t retrieve chart data for song "${playParams.targetSong.songName}" at difficulty "$difficulty" and variation "$variation"';
+        }
         var instPath:String = targetChart.getInstPath(playParams.targetInstrumental);
         var voicesPaths:Array<String> = targetChart.buildVoiceList();
 
@@ -104,9 +108,9 @@ class LoadingState extends MusicBeatSubState
       // library.types.set(symbolPath, SOUND);
       // @:privateAccess
       // library.pathGroups.set(symbolPath, [library.__cacheBreak(symbolPath)]);
-      var callback = callbacks.add('song:' + path);
+      var callback = callbacks?.add('song:' + path);
       Assets.loadSound(path).onComplete(function(_) {
-        callback();
+        if (callback != null) callback();
       });
     }
   }
@@ -119,9 +123,9 @@ class LoadingState extends MusicBeatSubState
       @:privateAccess
       if (!LimeAssets.libraryPaths.exists(library)) throw 'Missing library: ' + library;
 
-      var callback = callbacks.add('library:' + library);
+      var callback = callbacks?.add('library:' + library);
       Assets.loadLibrary(library).onComplete(function(_) {
-        callback();
+        if (callback != null) callback();
       });
     }
   }
@@ -176,6 +180,7 @@ class LoadingState extends MusicBeatSubState
   function onLoad():Void
   {
     // Stop the instrumental.
+    @:nullSafety(Off)
     if (stopMusic && FlxG.sound.music != null)
     {
       FlxG.sound.music.destroy();
@@ -196,7 +201,7 @@ class LoadingState extends MusicBeatSubState
 
   static function getSongPath():String
   {
-    return Paths.inst(PlayState.instance.currentSong.id);
+    return Paths.inst(PlayState.instance?.currentSong.id ?? throw 'Cannot retrieve song path');
   }
 
   static var stageDirectory:String = "shared";
@@ -213,7 +218,7 @@ class LoadingState extends MusicBeatSubState
     var daChart:Null<SongDifficulty> = params.targetSong.getDifficulty(params.targetDifficulty ?? Constants.DEFAULT_DIFFICULTY,
       params.targetVariation ?? Constants.DEFAULT_VARIATION);
 
-    var daStage = funkin.data.stage.StageRegistry.instance.fetchEntry(daChart?.stage ?? Constants.DEFAULT_STAGE);
+    var daStage:Null<Stage> = funkin.data.stage.StageRegistry.instance.fetchEntry(daChart?.stage ?? Constants.DEFAULT_STAGE);
     stageDirectory = daStage?._data?.directory ?? "shared";
     Paths.setCurrentLevel(stageDirectory);
 
@@ -248,6 +253,7 @@ class LoadingState extends MusicBeatSubState
     }
     #else
     // All assets preloaded, switch directly to play state (defualt on other targets).
+    @:nullSafety(Off)
     if (shouldStopMusic && FlxG.sound.music != null)
     {
       FlxG.sound.music.destroy();
@@ -256,7 +262,7 @@ class LoadingState extends MusicBeatSubState
 
     // Load and cache the song's charts.
     // Don't do this if we already provided the music and charts.
-    if (params?.targetSong != null && !params.overrideMusic)
+    if (!(params.overrideMusic ?? false))
     {
       params.targetSong.cacheCharts(true);
     }
@@ -410,7 +416,7 @@ class LoadingState extends MusicBeatSubState
     var libraryPaths = LimeAssets.libraryPaths;
     if (libraryPaths.exists(id))
     {
-      path = libraryPaths[id];
+      path = libraryPaths[id] ?? path;
       rootPath = Path.directory(path);
     }
     else
@@ -461,17 +467,18 @@ class LoadingState extends MusicBeatSubState
   }
 }
 
+@:nullSafety
 class MultiCallback
 {
   public var callback:Void->Void;
-  public var logId:String = null;
+  public var logId:Null<String>;
   public var length(default, null) = 0;
   public var numRemaining(default, null) = 0;
 
   var unfired = new Map<String, Void->Void>();
   var fired = new Array<String>();
 
-  public function new(callback:Void->Void, logId:String = null)
+  public function new(callback:Void->Void, ?logId:String)
   {
     this.callback = callback;
     this.logId = logId;
@@ -482,8 +489,7 @@ class MultiCallback
     id = '$length:$id';
     length++;
     numRemaining++;
-    var func:Void->Void = null;
-    func = function() {
+    var func:Void->Void = function() {
       if (unfired.exists(id))
       {
         unfired.remove(id);

matthewfro181818/Funkin-Contrib

source/funkin/ui/transition/LoadingState.hx
@@ -12,6 +12,7 @@ import funkin.graphics.shaders.ScreenWipeShader;
 import funkin.play.PlayState;
 import funkin.play.PlayStatePlaylist;
 import funkin.play.song.Song.SongDifficulty;
+import funkin.play.stage.Stage;
 import haxe.io.Path;
 import lime.app.Future;
 import lime.app.Promise;
@@ -21,6 +22,7 @@ import lime.utils.Assets as LimeAssets;
 import openfl.filters.ShaderFilter;
 import openfl.utils.Assets as OpenFLAssets;
 
+@:nullSafety
 class LoadingState extends MusicBeatSubState
 {
   inline static var MIN_TIME = 1.0;
@@ -30,18 +32,21 @@ class LoadingState extends MusicBeatSubState
   var target:NextState;
   var playParams:Null<PlayStateParams>;
   var stopMusic:Bool = false;
-  var callbacks:MultiCallback;
+  var callbacks:Null<MultiCallback>;
   var danceLeft:Bool = false;
 
   var loadBar:FlxSprite;
   var funkay:FlxSprite;
 
-  function new(target:NextState, stopMusic:Bool, playParams:Null<PlayStateParams> = null)
+  function new(target:NextState, stopMusic:Bool, ?playParams:PlayStateParams)
   {
     super();
     this.target = target;
     this.playParams = playParams;
     this.stopMusic = stopMusic;
+
+    this.loadBar = new FunkinSprite(0, FlxG.height - 20).makeSolidColor(0, 10, 0xFFff16d2);
+    this.funkay = FunkinSprite.create('funkay');
   }
 
   override function create():Void
@@ -49,14 +54,12 @@ class LoadingState extends MusicBeatSubState
     var bg:FunkinSprite = new FunkinSprite().makeSolidColor(FlxG.width, FlxG.height, 0xFFcaff4d);
     add(bg);
 
-    funkay = FunkinSprite.create('funkay');
     funkay.setGraphicSize(0, FlxG.height);
     funkay.updateHitbox();
     add(funkay);
     funkay.scrollFactor.set();
     funkay.screenCenter();
 
-    loadBar = new FunkinSprite(0, FlxG.height - 20).makeSolidColor(0, 10, 0xFFff16d2);
     add(loadBar);
 
     initSongsManifest().onComplete(function(lib) {
@@ -66,15 +69,21 @@ class LoadingState extends MusicBeatSubState
       if (playParams != null)
       {
         // Load and cache the song's charts.
-        if (playParams.targetSong != null)
+        if (playParams.targetSong == null)
         {
-          playParams.targetSong.cacheCharts(true);
+          throw 'Invalid parameter: Target song should not be null';
         }
+
+        playParams.targetSong.cacheCharts(true);
 
         // Preload the song for the play state.
         var difficulty:String = playParams.targetDifficulty ?? Constants.DEFAULT_DIFFICULTY;
         var variation:String = playParams.targetVariation ?? Constants.DEFAULT_VARIATION;
-        var targetChart:SongDifficulty = playParams.targetSong?.getDifficulty(difficulty, variation);
+        var targetChart:Null<SongDifficulty> = playParams.targetSong.getDifficulty(difficulty, variation);
+        if (targetChart == null)
+        {
+          throw 'Couldn\'t retrieve chart data for song "${playParams.targetSong.songName}" on difficulty "$difficulty" and variation "$variation"';
+        }
         var instPath:String = targetChart.getInstPath(playParams.targetInstrumental);
         var voicesPaths:Array<String> = targetChart.buildVoiceList();
 
@@ -105,9 +114,9 @@ class LoadingState extends MusicBeatSubState
       // library.types.set(symbolPath, SOUND);
       // @:privateAccess
       // library.pathGroups.set(symbolPath, [library.__cacheBreak(symbolPath)]);
-      var callback = callbacks.add('song:' + path);
+      var callback = callbacks?.add('song:' + path);
       Assets.loadSound(path).onComplete(function(_) {
-        callback();
+        if (callback != null) callback();
       });
     }
   }
@@ -120,9 +129,9 @@ class LoadingState extends MusicBeatSubState
       @:privateAccess
       if (!LimeAssets.libraryPaths.exists(library)) throw 'Missing library: ' + library;
 
-      var callback = callbacks.add('library:' + library);
+      var callback = callbacks?.add('library:' + library);
       Assets.loadLibrary(library).onComplete(function(_) {
-        callback();
+        if (callback != null) callback();
       });
     }
   }
@@ -177,6 +186,7 @@ class LoadingState extends MusicBeatSubState
   function onLoad():Void
   {
     // Stop the instrumental.
+    @:nullSafety(Off)
     if (stopMusic && FlxG.sound.music != null)
     {
       FlxG.sound.music.destroy();
@@ -197,7 +207,7 @@ class LoadingState extends MusicBeatSubState
 
   static function getSongPath():String
   {
-    return Paths.inst(PlayState.instance.currentSong.id);
+    return Paths.inst(PlayState.instance?.currentSong.id ?? throw 'Cannot retrieve song path');
   }
 
   static var stageDirectory:String = "shared";
@@ -211,10 +221,10 @@ class LoadingState extends MusicBeatSubState
    */
   public static function loadPlayState(params:PlayStateParams, shouldStopMusic = false, asSubState = false, ?onConstruct:PlayState->Void):Void
   {
-    var daChart:Null<SongDifficulty> = params.targetSong.getDifficulty(params.targetDifficulty ?? Constants.DEFAULT_DIFFICULTY,
+    var daChart:Null<SongDifficulty> = params.targetSong?.getDifficulty(params.targetDifficulty ?? Constants.DEFAULT_DIFFICULTY,
       params.targetVariation ?? Constants.DEFAULT_VARIATION);
 
-    var daStage = funkin.data.stage.StageRegistry.instance.fetchEntry(daChart?.stage ?? Constants.DEFAULT_STAGE);
+    var daStage:Null<Stage> = funkin.data.stage.StageRegistry.instance.fetchEntry(daChart?.stage ?? Constants.DEFAULT_STAGE);
     stageDirectory = daStage?._data?.directory ?? "shared";
     Paths.setCurrentLevel(stageDirectory);
 
@@ -249,6 +259,7 @@ class LoadingState extends MusicBeatSubState
     }
     #else
     // All assets preloaded, switch directly to play state (defualt on other targets).
+    @:nullSafety(Off)
     if (shouldStopMusic && FlxG.sound.music != null)
     {
       FlxG.sound.music.destroy();
@@ -257,7 +268,7 @@ class LoadingState extends MusicBeatSubState
 
     // Load and cache the song's charts.
     // Don't do this if we already provided the music and charts.
-    if (params?.targetSong != null && !params.overrideMusic)
+    if (!(params.overrideMusic ?? false))
     {
       params.targetSong.cacheCharts(true);
     }
@@ -272,7 +283,8 @@ class LoadingState extends MusicBeatSubState
       var songDifficulty = params.targetSong.getDifficulty(params.targetDifficulty, params.targetVariation);
       if (songDifficulty != null)
       {
-        var noteStyle = NoteStyleRegistry.instance.fetchEntry(songDifficulty.noteStyle);
+        var noteStyle = NoteStyleRegistry.instance.fetchEntry(songDifficulty.noteStyle ?? '');
+        if (noteStyle == null) noteStyle = NoteStyleRegistry.instance.fetchDefault();
         FunkinMemory.cacheNoteStyle(noteStyle);
       }
 
@@ -439,7 +451,7 @@ class LoadingState extends MusicBeatSubState
     var libraryPaths = LimeAssets.libraryPaths;
     if (libraryPaths.exists(id))
     {
-      path = libraryPaths[id];
+      path = libraryPaths[id] ?? path;
       rootPath = Path.directory(path);
     }
     else
@@ -490,17 +502,18 @@ class LoadingState extends MusicBeatSubState
   }
 }
 
+@:nullSafety
 class MultiCallback
 {
   public var callback:Void->Void;
-  public var logId:String = null;
+  public var logId:Null<String>;
   public var length(default, null) = 0;
   public var numRemaining(default, null) = 0;
 
   var unfired = new Map<String, Void->Void>();
   var fired = new Array<String>();
 
-  public function new(callback:Void->Void, logId:String = null)
+  public function new(callback:Void->Void, ?logId:String)
   {
     this.callback = callback;
     this.logId = logId;
@@ -511,8 +524,7 @@ class MultiCallback
     id = '$length:$id';
     length++;
     numRemaining++;
-    var func:Void->Void = null;
-    func = function() {
+    var func:Void->Void = function() {
       if (unfired.exists(id))
       {
         unfired.remove(id);