455a13622f0879a8b3cd55e9a21f9f72941a51fb
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-09-12 06:09:10 -0400

    Update import.hx

--- source/funkin/import.hx
+++ source/funkin/import.hx
@@ -9,6 +9,7 @@ import funkin.Preferences;
 import flixel.FlxG; // This one in particular causes a compile error if you're using macros.
 import flixel.system.debug.watch.Tracker;
 
+
 // These are great.
 using Lambda;
 using StringTools;
@@ -23,4 +24,5 @@ using funkin.util.tools.MapTools;
 using funkin.util.tools.SongEventDataArrayTools;
 using funkin.util.tools.SongNoteDataArrayTools;
 using funkin.util.tools.StringTools;
+import funkin.ui.options.*;
 #end
